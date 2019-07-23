b3f3def71f65db553c5308563173e3d9bd98d09a
17010635 <17010635@myrp.edu.sg>
2019-07-23 15:55:09 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/democooldrawer/Drawer.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.c347.democooldrawer;
+
+import android.widget.ImageView;
+
+public class Drawer {
+    private String name;
+    private int ivIcon;
+
+    public Drawer(String name, int ivIcon) {
+        this.name = name;
+        this.ivIcon = ivIcon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIvIcon() {
+        return ivIcon;
+    }
+
+    public void setIvIcon(int ivIcon) {
+        this.ivIcon = ivIcon;
+    }
+}
